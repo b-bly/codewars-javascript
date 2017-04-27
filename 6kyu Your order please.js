e7b2358a375b17c112ffa1ae92fac0adf4737d1a
Brendt <brendt.bly@outlook.com>
2017-04-27 17:56:30 -0500

    Create 6kyu Your order please.js

---
+++ 6kyu Your order please.js
@@ -0,0 +1,25 @@
+/*
+Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+
+For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
+
+*/
+
+function order(words){
+  // ...
+  if (words) {
+    var arr = words.split(" ");
+    var sortedArr = arr.slice();
+    var index = 0;
+    for (var i = 0; i < arr.length; i++) {
+        index = arr[i].match(/\d+/g)[0] - 1;
+        sortedArr[index] = arr[i];
+    }
+    return sortedArr.join(' ');
+  }
+  return "";
+}
